df88ad13e7c8a2cadb3eedb96399c0348b9b4e64
Brent Guistwite <brentguistwite@gmail.com>
2018-02-05 12:39:51 -0500

    add select element to issue form

--- src/components/IssueForm.js
+++ src/components/IssueForm.js
@@ -8,8 +8,28 @@ export default function IssueForm() {
        <label htmlFor="trackingNumber">Tracking Number
        </label>
        <input name="trackingNumber" id="trackingNumber"/>
-     </div> 
-       
+     </div>
+     <div className="form-input">
+      <label htmlFor="issue">What is your issue?
+      </label>
+      <select name="issue" id="issue">
+        <option value="not-delivered">
+        My delivery hasnt arrived
+        </option>
+        <option value="wrong-item">
+        The wrong item was delivered
+        </option>
+        <option value="missing-part">
+        Part of my order was missing
+        </option>
+        <option value="damaged">
+        Some of my order arrived damaged
+        </option>
+        <option value="other">
+        Other (give details below)
+        </option>
+        </select>
+     </div>         
     </form>
   )
 }
